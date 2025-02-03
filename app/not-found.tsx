6b782d20b4022a32bdc1dc42e799d107c405f925
syncler-agr <syncagr@gmail.com>
2025-02-03 14:38:55 -0300

    testando os comando

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+
+export default function NotFound() {
+    return (
+   <div>     
+     <h1>não encotrado</h1>
+
+     <a href="/">Voltar para Home</a>
+     </div>
+    );
+  }
+  
---
+++ app/vagas/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Vagas() {
+    return (
+     <h1>Listagem de Vagas</h1>
+    );
+  }
+  
